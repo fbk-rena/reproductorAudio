333eb2dc563f77e76a785be0c10a3727a8613ee1
Rena <Rena>
2017-03-28 01:23:37 -0600

    Estilo agregado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-alert("Hola");
+
 var music = document.getElementById("playlist")
+
+function myFunction() {
+    document.body.style.backgroundImage = "url(http://s3.favim.com/610/141125/hudson-katy-perry-katycat-prism-Favim.com-2263333.jpg)";
+}
 
 function play(){
     music.play();
@@ -12,6 +16,10 @@ function pause(){
 function stop(){
     music.load();
 }
+
+function volumen(){
+    music.volume.innerText;
+} 
 
 function volumenUp(){
     music.volume+=0.1;
