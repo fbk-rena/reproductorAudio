0ffacf46ed1dc42b5556b0786848d8fab157101e
Rena <Rena>
2017-03-27 23:36:04 -0600

    proyecto funcional

---
+++ js/main.js
@@ -0,0 +1,31 @@
+alert("Hola");
+var music = document.getElementById("playlist")
+
+function play(){
+    music.play();
+}
+
+function pause(){
+    music.pause();
+}
+
+function stop(){
+    music.load();
+}
+
+function volumenUp(){
+    music.volume+=0.1;
+}
+
+function volumenDown(){
+    music.volume-=0.1;
+}
+
+function backward(){
+    music.currentTime -= 5;
+}
+
+function forward(){
+    music.currentTime += 5;
+}
+  
